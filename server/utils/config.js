1fe25abba56eafdde98b35310313a9df8e9af85c
RobCso <robcso@DESKTOP-AA47BN0.localdomain>
2023-02-17 16:26:22 +0000

    set up database schema

--- server/utils/config.js
+++ server/utils/config.js
@@ -13,8 +13,9 @@ function createDatabaseUrl() {
 	}
 	const host = process.env.DB_HOST ?? "localhost";
 	const name = process.env.DB_NAME ?? "cyf";
-	const password = process.env.DB_PASS ?? process.env.DB_PASSWORD ?? "";
+	const password = process.env.DB_PASS ?? process.env.DB_PASSWORD ?? "postgres";
 	const port = process.env.DB_PORT ?? "5432";
-	const username = process.env.DB_USER ?? process.env.DB_USERNAME ?? "";
+	const username =
+		process.env.DB_USER ?? process.env.DB_USERNAME ?? "code_crafters";
 	return `postgres://${username}:${password}@${host}:${port}/${name}`;
 }
