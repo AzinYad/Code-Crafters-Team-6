7981d34528e61441264051b7727d6ebf84552a5f
SEBLE <kkahsay80@gmail.com>
2023-02-20 17:36:26 +0000

    Ceated Description file in Client

---
+++ client/src/commponet/Description.js
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+
+import Description from "./Description";
+
+const Description = () =>{
+const [description, setDescription] = useState("")
+
+const 
+
+}
+
+export default Description;
