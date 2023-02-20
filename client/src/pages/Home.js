3d82eebc0583697e9300c91c88831f36df61eda4
SEBLE <kkahsay80@gmail.com>
2023-02-20 17:34:17 +0000

    Added card component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,43 +1,50 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-
+// import { useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
 import "./Home.css";
-import logo from "./logo.svg";
+
+
+import Card from "../commponet/Card";
 
 export function Home() {
-	const [message, setMessage] = useState("Loading...");
+	// const [message, setMessage] = useState("Loading...");
 
-	useEffect(() => {
-		fetch("/api")
-			.then((res) => {
-				if (!res.ok) {
-					throw new Error(res.statusText);
-				}
-				return res.json();
-			})
-			.then((body) => {
-				setMessage(body.message);
-			})
-			.catch((err) => {
-				console.error(err);
-			});
-	}, []);
+	// useEffect(() => {
+	// 	fetch("/api")
+	// 		.then((res) => {
+	// 			if (!res.ok) {
+	// 				throw new Error(res.statusText);
+	// 			}
+	// 			return res.json();
+	// 		})
+	// 		.then((body) => {
+	// 			setMessage(body.message);
+	// 		})
+	// 		.catch((err) => {
+	// 			console.error(err);
+	// 		});
+	// }, []);
 
 	return (
-		<main role="main">
+		// <main role="main">
 			<div>
-				<img
-					className="logo"
-					data-qa="logo"
-					src={logo}
+{/* <Card /> */}
+
+{/* <img
+className="logo"
+data-qa="logo"
+src={logo}
+
 					alt="Just the React logo"
 				/>
 				<h1 className="message" data-qa="message">
 					{message}
 				</h1>
-				<Link to="/about/this/site">About</Link>
+				<Link to="/about/this/site">About</Link> */}
+				{/* <FirstPage /> */}
+				<Card />
 			</div>
-		</main>
+		// </main>
 	);
 }
 
