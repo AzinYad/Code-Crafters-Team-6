412e037539bcafe8e0830e2f551f3fded762d2e5
AzinYad <azin.yadegari@gmail.com>
2023-02-21 13:46:18 +0000

    Addressed feedback from code review

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,9 @@
 import { Route, Routes } from "react-router-dom";
-
-// import About from "./pages/About";
-
 import Home from "./pages/Home";
 
 const App = () => (
 	<Routes>
-		<Route path="/home" element={<Home />} />
+		<Route path="/" element={<Home />} />
 	</Routes>
 );
 
