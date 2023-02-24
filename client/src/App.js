c6678e871516cd96d0dcf49006448d5028fde6ec
AzinYad <azin.yadegari@gmail.com>
2023-02-24 23:17:02 +0000

    Share energiser link

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Energisers from "./pages/Energisers.js";
+import EnergiserDetail from "./pages/EnergiserDetail";
 
 const App = () => (
 	<Routes>
 		<Route path="/" element={<Home />} />
 		<Route path="/home" element={<Home />} />
 		<Route path="/energizers" element={<Energisers />} />
+		<Route path="/energizer-detail/:id" element={<EnergiserDetail />} />
 	</Routes>
 );
 export default App;
