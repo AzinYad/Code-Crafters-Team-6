5957c4aa4a93e9e0e201e84561e922653d99c031
SEBLE <kkahsay80@gmail.com>
2023-02-28 14:19:40 +0000

    added import

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Energisers from "./pages/Energisers.js";
 import EnergiserDetail from "./pages/EnergiserDetail";
-
+import CreateEnergiser from "./pages/CreateEnergiser";
 const App = () => (
 	<Routes>
 		<Route path="/" element={<Home />} />
 		<Route path="/home" element={<Home />} />
 		<Route path="/energizers" element={<Energisers />} />
 		<Route path="/energizers/:id" element={<EnergiserDetail />} />
+		<Route path="/create-energizer" element={<CreateEnergiser />} />
 	</Routes>
 );
 export default App;
