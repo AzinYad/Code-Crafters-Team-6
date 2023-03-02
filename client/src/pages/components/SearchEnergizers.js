8fdceb036aca314b590bfeb91dbf95637923c07a
CYF <braz.justinas@gmail.com>
2023-03-02 20:51:09 +0000

    using filter

---
+++ client/src/pages/components/SearchEnergizers.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+function SearchEnergizers({ energizers }) {
+  const [search,  setSearchTerm] = useState("");
+  [filteredEnergizers, setFilteredEnergizers] = useState(energizers);
+
+  const handleSearch = (event) => {
+    const term = event.target.value.toLowerCase();
+    const filtered = energizers.filter((energizer) =>
+      energizer.name.toLowerCase().includes(term)
+    );
+    setFilteredEnergizers(filtered);
+    setSearchTerm(term);
+  };
+
+  return (
+    <div>
+      <input type="text" placeholder="Search energizers" onChange={handleSearch} />
+      {filteredEnergizers.map((energizer) => (
+        <div key={energizer.name}>
+          <h2>{energizer.name}</h2>
+          <p>{energizer.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default SearchEnergizers;
