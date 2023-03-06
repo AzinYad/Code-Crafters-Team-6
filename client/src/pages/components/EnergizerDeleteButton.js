bd04169cc5cc174919e74182478c6f1819b5c7af
SEBLE <kkahsay80@gmail.com>
2023-03-06 16:10:27 +0000

    added some code

--- client/src/pages/components/EnergizerDeleteButton.js
+++ client/src/pages/components/EnergizerDeleteButton.js
@@ -1,37 +1,37 @@
 import React, { useState } from "react";
 
 function EnergizerDeleteButton({ energizerId }) {
-  const [isDeleting, setIsDeleting] = useState(false);
+	const [isDeleting, setIsDeleting] = useState(false);
 
+	const handleDelete = async (e) => {
+		e.preventDefault();
+		alert("Do you want to delete this energizer?");
+		try {
+			setIsDeleting(true);
+			const response = await fetch(`/api/energizers/${energizerId}`, {
+				method: "DELETE",
+			});
+			if (!response.ok) {
+				const errorText = await response.text();
+				throw new Error(errorText);
+			}
 
-  const handleDelete = async () => {
-    try {
-      setIsDeleting(true);
-      const response = await fetch(`/api/energizers/${energizerId}`, {
-        method: "DELETE",
-      });
-      if (!response.ok) {
-        const errorText = await response.text();
-        throw new Error(errorText);
-      }
+			window.history.back();
+		} catch (error) {
+			console.error(error);
+			alert("Failed to delete energizer");
+		} finally {
+			setIsDeleting(false);
+		}
+	};
 
-    window.location.href = "/energizers";
-
-    } catch (error) {
-      console.error(error);
-      alert("Failed to delete energizer");
-    } finally {
-      setIsDeleting(false);
-    }
-  };
-
-  return (
-    <form onSubmit={handleDelete}>
-    <button type="submit" disabled={isDeleting}>
-      {isDeleting ? "Deleting..." : "Delete"}
-    </button>
-  </form>
-  );
+	return (
+		<form onSubmit={handleDelete}>
+			<button type="submit" disabled={isDeleting}>
+				{isDeleting ? "Deleting..." : "Delete"}
+			</button>
+		</form>
+	);
 }
 
-export default  EnergizerDeleteButton;
+export default EnergizerDeleteButton;
