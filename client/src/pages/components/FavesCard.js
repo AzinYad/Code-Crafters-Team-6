8a7764ba7c2d975a0c6e9e70f7872fb923162af4
AzinYad <azin.yadegari@gmail.com>
2023-03-09 17:47:42 +0000

    solve conflicts

--- client/src/pages/components/FavesCard.js
+++ client/src/pages/components/FavesCard.js
@@ -3,17 +3,15 @@ import { Link } from "react-router-dom";
 
 function FavesCard({ item }) {
 	return (
-<Link to={`/energizers/${item.id}`} className="info-sec-name">
-		<div className="fave-energizer-box">
-			<section className="fave-energizer">{item.description}</section>
-			<section className="fave-energizer-info">
-				<h3>
-					{item.name}
-				</h3>
-				<p>rating</p>
-			</section>
-		</div>
-</Link>
+		<Link to={`/energizers/${item.id}`} className="info-sec-name">
+			<div className="fave-energizer-box">
+				<section className="fave-energizer">{item.description}</section>
+				<section className="fave-energizer-info">
+					<h3>{item.name}</h3>
+					<p>Rate:{item.rating}</p>
+				</section>
+			</div>
+		</Link>
 	);
 }
 export default FavesCard;
