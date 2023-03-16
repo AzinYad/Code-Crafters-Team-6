5c07dd36ac1f404ef53acc085fa931d040d9acb3
AzinYad <azin.yadegari@gmail.com>
2023-03-16 19:09:08 +0000

    fixing NavLink issue

--- client/src/pages/components/Navbar.js
+++ client/src/pages/components/Navbar.js
@@ -1,34 +1,40 @@
 import React from "react";
 import Logo from "../Logo/logo.png";
-import { NavLink, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { FaSearch, FaUserCircle } from "react-icons/fa";
 
 function Navbar({ showSearch }) {
-    return (
-        <div>
-            <nav className="nav-bar">
-                {showSearch ? (
-                    <div className="search-wrapper nav-sub-wrapper">
-                        <FaSearch className="magnifier" />
-                        <input className="search-input" type="search" placeholder="Search..." />
-                    </div>
-                ) : (
-                    <section className="logo-sec">
-                        <img className="logo" src={Logo} alt="logo" />
-                    </section>
-                )}
-                <div className="pages-wrapper nav-sub-wrapper">
-                    <NavLink className="page" activeClassName="active-page" exact to="/home">Home</NavLink>
-                    <NavLink className="page create-energizer" activeClassName="active-page" to="/create-energizer">Create Energizer</NavLink>
-                    <NavLink className="page" activeClassName="active-page" to="/energizers">Energizers</NavLink>
-                </div>
-                <Link className="login-wrapper nav-sub-wrapper" to="/login">
-                    <FaUserCircle className="login-icon" />
-                    Log in
-                </Link>
-            </nav>
+  return (
+    <div>
+      <nav className="nav-bar">
+        {showSearch ? (
+          <div className="search-wrapper nav-sub-wrapper">
+            <FaSearch className="magnifier" />
+            <input className="search-input" type="search" placeholder="Search..." />
+          </div>
+        ) : (
+          <section className="logo-sec">
+            <img className="logo" src={Logo} alt="logo" />
+          </section>
+        )}
+        <div className="pages-wrapper nav-sub-wrapper">
+          <Link className="page" to="/home">
+            Home
+          </Link>
+          <Link className="page create-energizer" to="/create-energizer">
+            Create Energizer
+          </Link>
+          <Link className="page" to="/energizers">
+            Energizers
+          </Link>
         </div>
-    );
+        <Link className="login-wrapper nav-sub-wrapper" to="/login">
+          <FaUserCircle className="login-icon" />
+          Log in
+        </Link>
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar;
